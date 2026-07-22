a5fbbf301d93d572cf644f36d542f7ab2a86a235
sabina shakya <sabinashakya099@gmail.com>
2026-07-22 21:38:00 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//1.Program to Show Inheritance in C#
+using System;
+
+class Animal
+{
+    public void Sound()
+    {
+        Console.WriteLine("Animal makes sound");
+    }
+}
+
+class Dog : Animal
+{
+    public void Bark()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Dog d = new Dog();
+
+        d.Sound();
+        d.Bark();
+    }
+}
